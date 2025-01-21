8ec8f0e23e9eabb63dce89a39d0cde4baeec6ea4
delvering17 <delvering17@gmail.com>
2025-01-21 23:40:01 +0900

    Chapter 01: 명확한 이름으로 변경

--- src/main/java/ch01/Statement.java
+++ src/main/java/ch01/Statement.java
@@ -34,22 +34,22 @@ public class Statement {
         return result;
     }
 
-    private int amountFor(Performance perf, Play play) {
+    private int amountFor(Performance aPerformance, Play play) {
         int result = 0;
 
         switch (play.type()) {
             case "tragedy": // 비극
                 result = 40000;
-                if (perf.audience() > 30) {
-                    result += 1000 * (perf.audience() - 30);
+                if (aPerformance.audience() > 30) {
+                    result += 1000 * (aPerformance.audience() - 30);
                 }
                 break;
             case "comedy": // 희극
                 result = 30000;
-                if (perf.audience() > 20) {
-                    result += 10000 + 500 * (perf.audience() - 20);
+                if (aPerformance.audience() > 20) {
+                    result += 10000 + 500 * (aPerformance.audience() - 20);
                 }
-                result += 300 * perf.audience();
+                result += 300 * aPerformance.audience();
                 break;
             default:
                 throw new RuntimeException("알 수 없는 장르: " + play.type());
