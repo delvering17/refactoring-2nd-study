fa5a915f39110b2b3f21d76e41ed325daa170062
delvering17 <delvering17@gmail.com>
2025-01-21 23:35:56 +0900

    Chapter 01: statement 함수 추출하기

--- src/main/java/ch01/Statement.java
+++ src/main/java/ch01/Statement.java
@@ -16,23 +16,7 @@ public class Statement {
             Play play = plays.get(perf.playID());
             int thisAmount = 0;
 
-            switch (play.type()) {
-                case "tragedy": // 비극
-                    thisAmount = 40000;
-                    if (perf.audience() > 30) {
-                        thisAmount += 1000 * (perf.audience() - 30);
-                    }
-                    break;
-                case "comedy": // 희극
-                    thisAmount = 30000;
-                    if (perf.audience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience() - 20);
-                    }
-                    thisAmount += 300 * perf.audience();
-                    break;
-                default:
-                    throw new RuntimeException("알 수 없는 장르: " + play.type());
-            }
+            thisAmount = amountFor(perf, play);
 
             // 포인트를 적립한다.
             volumeCredits += Math.max(perf.audience() - 30, 0);
@@ -48,6 +32,30 @@ public class Statement {
         result += "총액: " + format.format(totalAmount/100) + "\n";
         result += "적립 포인트: " + volumeCredits + "점\n";
         return result;
+    }
+
+    private int amountFor(Performance perf, Play play) {
+        int thisAmount = 0;
+
+        switch (play.type()) {
+            case "tragedy": // 비극
+                thisAmount = 40000;
+                if (perf.audience() > 30) {
+                    thisAmount += 1000 * (perf.audience() - 30);
+                }
+                break;
+            case "comedy": // 희극
+                thisAmount = 30000;
+                if (perf.audience() > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience() - 20);
+                }
+                thisAmount += 300 * perf.audience();
+                break;
+            default:
+                throw new RuntimeException("알 수 없는 장르: " + play.type());
+        }
+
+        return thisAmount;
     }
 
     public static void main(String[] args) {
