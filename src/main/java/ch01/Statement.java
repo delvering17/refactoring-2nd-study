c329a6b4c98d3b99bdf94b8fd74ccea385fc19e1
delvering17 <delvering17@gmail.com>
2025-01-31 21:27:13 +0900

    Chapter 01: totalVolumeCredits 변수 인라인

--- src/main/java/ch01/Statement.java
+++ src/main/java/ch01/Statement.java
@@ -23,10 +23,9 @@ public class Statement {
             result += "    " + playFor(perf).name() + ": " + usd(amountFor(perf)) + " (" + perf.audience() + "석)\n";
             totalAmount += amountFor(perf);
         }
-        int volumeCredits = totalVolumeCredits();
 
         result += "총액: " + usd(totalAmount) + "\n";
-        result += "적립 포인트: " + volumeCredits + "점\n";
+        result += "적립 포인트: " + totalVolumeCredits() + "점\n";
         return result;
     }
 
