d8e45eb86f3707eb10072ddee4dd813559234e08
delvering17 <delvering17@gmail.com>
2025-01-31 21:23:55 +0900

    Chapter 01: volumeCredits 반복문 쪼개기

--- src/main/java/ch01/Statement.java
+++ src/main/java/ch01/Statement.java
@@ -17,12 +17,14 @@ public class Statement {
         String result = "청구 내역(고객명: " + invoice.customer() + ")\n";
 
         for (Performance perf : invoice.performances()) {
-            volumeCredits = volumeCreditsFor(perf);
-
             // 청구 내역을 출력한다.
             result += "    " + playFor(perf).name() + ": " + usd(amountFor(perf)) + " (" + perf.audience() + "석)\n";
             totalAmount += amountFor(perf);
         }
+        for (Performance perf : invoice.performances()) {
+            volumeCredits = volumeCreditsFor(perf);
+        }
+
         result += "총액: " + usd(totalAmount) + "\n";
         result += "적립 포인트: " + volumeCredits + "점\n";
         return result;
