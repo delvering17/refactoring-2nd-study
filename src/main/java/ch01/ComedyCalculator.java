1e7d886f4cf0371ac404405df0ef62364a4a3d08
delvering17 <delvering17@gmail.com>
2025-02-04 02:16:02 +0900

    Chapter 01: amount comedy 조건부 로직을 다형성으로 바꾸기

--- src/main/java/ch01/ComedyCalculator.java
+++ src/main/java/ch01/ComedyCalculator.java
@@ -4,4 +4,15 @@ public class ComedyCalculator extends PerformanceCalculator {
     public ComedyCalculator(Performance aPerformance, Play aPlay) {
         super(aPerformance, aPlay);
     }
+
+    @Override
+    public int amount() {
+        int result = 30000;
+        if (performance.audience() > 20) {
+            result += 10000 + 500 * (performance.audience() - 20);
+        }
+        result += 300 * performance.audience();
+
+        return result;
+    }
 }
--- src/main/java/ch01/PerformanceCalculator.java
+++ src/main/java/ch01/PerformanceCalculator.java
@@ -15,23 +15,7 @@ public class PerformanceCalculator {
     }
 
     public int amount() {
-        int result = 0;
-
-        switch (play.type()) {
-            case "tragedy": // 비극
-                throw new RuntimeException("오류 발생");
-            case "comedy": // 희극
-                result = 30000;
-                if (performance.audience() > 20) {
-                    result += 10000 + 500 * (performance.audience() - 20);
-                }
-                result += 300 * performance.audience();
-                break;
-            default:
-                throw new RuntimeException("알 수 없는 장르: " + play.type());
-        }
-
-        return result;
+        throw new UnsupportedOperationException("서브클래스에서 처리하도록 설계되었습니다.");
     }
 
     public int volumeCredits() {
