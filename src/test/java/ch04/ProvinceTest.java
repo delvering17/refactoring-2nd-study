a25891c96d9afb35e62c22df2dd91594c1fcb672
delvering17 <delvering17@gmail.com>
2025-03-17 00:50:41 +0900

    Chapter 04: 첫 번째 테스트 생산 부족분

---
+++ src/test/java/ch04/ProvinceTest.java
@@ -0,0 +1,22 @@
+package ch04;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ProvinceTest {
+
+    @Test
+    void shortfall() {
+        List<ProducerSaveRequest> producerSaveRequests = List.of(
+                new ProducerSaveRequest("Byzantium", 10L, 9L),
+                new ProducerSaveRequest("Attalia", 12L, 10L),
+                new ProducerSaveRequest("Sinope", 10L, 6L)
+        );
+        Province province = new Province("Asia", 30L, 20L, producerSaveRequests);
+        assertThat(province.shortfall()).isEqualTo(5L);
+    }
+
+}
