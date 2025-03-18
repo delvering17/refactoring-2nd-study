cfb844b068fea99e7eddf7653ef03cdabb0db0e1
delvering17 <delvering17@gmail.com>
2025-03-19 01:12:58 +0900

    Chapter 04: @BeforeEach로 픽스처 생성

--- src/test/java/ch04/ProvinceTest.java
+++ src/test/java/ch04/ProvinceTest.java
@@ -1,5 +1,6 @@
 package ch04;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,25 +9,25 @@ import static org.assertj.core.api.Assertions.*;
 
 class ProvinceTest {
 
-    @Test
-    void shortfall() {
+    private Province province;
+
+    @BeforeEach
+    void beforeEach() {
         List<ProducerSaveRequest> producerSaveRequests = List.of(
                 new ProducerSaveRequest("Byzantium", 10L, 9L),
                 new ProducerSaveRequest("Attalia", 12L, 10L),
                 new ProducerSaveRequest("Sinope", 10L, 6L)
         );
-        Province province = new Province("Asia", 30L, 20L, producerSaveRequests);
+        province = new Province("Asia", 30L, 20L, producerSaveRequests);
+    }
+
+    @Test
+    void shortfall() {
         assertThat(province.shortfall()).isEqualTo(5L);
     }
 
     @Test
     void profit() {
-        List<ProducerSaveRequest> producerSaveRequests = List.of(
-                new ProducerSaveRequest("Byzantium", 10L, 9L),
-                new ProducerSaveRequest("Attalia", 12L, 10L),
-                new ProducerSaveRequest("Sinope", 10L, 6L)
-        );
-        Province province = new Province("Asia", 30L, 20L, producerSaveRequests);
         assertThat(province.profit()).isEqualTo(230);
     }
 
