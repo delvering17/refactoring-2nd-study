d87af11f1016de43b7c64562a96819ef91db38f3
delvering17 <delvering17@gmail.com>
2025-03-23 20:38:42 +0900

    Chapter 06 Extract Variable: quantityDiscount 변수 추출

--- src/main/java/ch06/ExtractVariable/ExtractVariable.java
+++ src/main/java/ch06/ExtractVariable/ExtractVariable.java
@@ -5,9 +5,10 @@ public class ExtractVariable {
     public double price(Order order) {
         // 가격(price) = 기본 가격 - 수량 할인 + 배송비
         int basePrice = order.quantity() * order.itemPrice();
+        double quantityDiscount = Math.max(0, order.quantity()) * order.itemPrice() * 0.05;
         return basePrice -
-                Math.max(0, order.quantity()) * order.itemPrice() * 0.05 +
-                Math.min(order.quantity() * order.itemPrice() * 0.1, 100);
+                quantityDiscount +
+                Math.min(basePrice * 0.1, 100);
     }
 
 }
