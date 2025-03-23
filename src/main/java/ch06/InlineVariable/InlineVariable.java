2d6832d401a68301276c5e6e2a5bc63ec285d745
delvering17 <delvering17@gmail.com>
2025-03-23 20:52:43 +0900

    Chapter 06 Inline Variable: 변수 인라인

--- src/main/java/ch06/InlineVariable/InlineVariable.java
+++ src/main/java/ch06/InlineVariable/InlineVariable.java
@@ -3,9 +3,8 @@ package ch06.InlineVariable;
 public class InlineVariable {
 
     public boolean inlineVariable(Order order) {
-        var basePrice = order.basePrice;
 
-        return basePrice > 1000;
+        return order.basePrice > 1000;
     }
 
     record Order(int basePrice) {}
