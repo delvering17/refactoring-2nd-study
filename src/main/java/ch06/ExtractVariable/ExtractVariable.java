47d1fbf3754f1244197e91b145c9bb90b7caf5fa
delvering17 <delvering17@gmail.com>
2025-03-23 20:42:53 +0900

    Chapter 06 Extract Variable: 클래스 안에서 변수가 아닌 함수로 추출

--- src/main/java/ch06/ExtractVariable/ExtractVariable.java
+++ src/main/java/ch06/ExtractVariable/ExtractVariable.java
@@ -4,10 +4,22 @@ public class ExtractVariable {
 
     public double price(Order order) {
         // 가격(price) = 기본 가격 - 수량 할인 + 배송비
-        int basePrice = order.quantity() * order.itemPrice();
-        double quantityDiscount = Math.max(0, order.quantity()) * order.itemPrice() * 0.05;
-        double shipping = Math.min(basePrice * 0.1, 100);
+        int basePrice = basePrice(order);
+        double quantityDiscount = quantityDiscount(order);
+        double shipping = getShipping(basePrice);
         return basePrice - quantityDiscount + shipping;
     }
 
+    public int basePrice(Order order) {
+        return order.quantity() * order.itemPrice();
+    }
+
+    public double quantityDiscount(Order order) {
+        return Math.max(0, order.quantity()) * order.itemPrice() * 0.05;
+    }
+
+
+    public double getShipping(int basePrice) {
+        return Math.min(basePrice * 0.1, 100);
+    }
 }
