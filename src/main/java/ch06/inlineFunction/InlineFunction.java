aa9f194956cd7ce46e44d6316c92720bb19d35cb
delvering17 <delvering17@gmail.com>
2025-03-23 20:18:45 +0900

    Chapter 06 Inline Function: 인라인 함수 1

--- src/main/java/ch06/inlineFunction/InlineFunction.java
+++ src/main/java/ch06/inlineFunction/InlineFunction.java
@@ -7,13 +7,14 @@ public class InlineFunction {
 
     public Map<String, String> reportLines(Customer aCustomer) {
         Map<String, String> lines = new HashMap<>();
+
+        lines.put("name", aCustomer.name());
         gatherCustomerData(lines, aCustomer);
 
         return lines;
     }
 
     private void gatherCustomerData(Map<String, String> out, Customer aCustomer) {
-        out.put("name", aCustomer.name());
         out.put("location", aCustomer.location());
     }
 
